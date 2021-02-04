ad2f2ad3295eecce887533fbb85bab436bde03a7
Luis Angel <santosangel748@gmail.com>
2021-02-04 17:27:23 -0600

    Creacion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UserController@index');
+Route::post('users', 'UserController@store')->name('users.store');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+
