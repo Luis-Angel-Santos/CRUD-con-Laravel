006dcbfb061e0ae34fb0f9b0aaa529b148309110
Luis Angel <santosangel748@gmail.com>
2021-02-04 18:00:22 -0600

    Creando la logica de los controladores y vistas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-
+        $users = User::latest() -> get();
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
-    public function store(){
-        
+    public function store(Request $request){
+        User::created([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        return back();
     }
-    public function destroy(){
-        
+    public function destroy(User $user){
+        $user->delete();
+        return back();
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 mx-auto">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>
+                                    <form action="{{route('users.destroy', $user)}}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input 
+                                            type="submit" 
+                                            value="Eliminar" 
+                                            class="btn btn-sm btn-danger"
+                                            onclick="return confirm('¿Esta Seguro?')">
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>        
+    </body>
+</html>
